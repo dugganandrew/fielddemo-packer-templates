3795ec5897f88831480cd67d0d40cded2f8e3679
dugganandrew <duggan.andrew@gmail.com>
2022-05-23 15:46:34 +0100

    Update win2016.ps1

--- scripts/win2016.ps1
+++ scripts/win2016.ps1
@@ -17,3 +17,12 @@ winrm set winrm/config/service/auth '@{Basic="true"}'
 # Reset auto logon count
 # https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
+
+#Configure NIC as Env has no DHCP
+$netadapter = Get-NetAdapter -Name Ethernet
+## Disable DHCP
+$netadapter | Set-NetIPInterface -DHCP Disabled
+## Configure the IP address and default gateway for "VM Network" in Env
+$netadapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress 10.59.93.15 -PrefixLength 24 -Type Unicast -DefaultGateway 10.59.93.1
+## Configure the DNS servers
+Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses ("10.30.15.200","10.30.55.200”)
