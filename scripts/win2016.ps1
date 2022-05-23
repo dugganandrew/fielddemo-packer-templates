5e40d27a42776df67a2831eb9989472918570f54
dugganandrew <duggan.andrew@gmail.com>
2022-05-23 16:35:38 +0100

    Update win2016.ps1

--- scripts/win2016.ps1
+++ scripts/win2016.ps1
@@ -19,10 +19,10 @@ winrm set winrm/config/service/auth '@{Basic="true"}'
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
 
 #Configure NIC as Env has no DHCP
-$netadapter = Get-NetAdapter -Name Ethernet
+$netadapter = Get-NetAdapter -Name Ethernet0
 ## Disable DHCP
 $netadapter | Set-NetIPInterface -DHCP Disabled
 ## Configure the IP address and default gateway for "VM Network" in Env
 $netadapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress 10.59.93.15 -PrefixLength 24 -Type Unicast -DefaultGateway 10.59.93.1
 ## Configure the DNS servers
-Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses ("10.30.15.200","10.30.55.200")
+Set-DnsClientServerAddress -InterfaceAlias Ethernet0 -ServerAddresses ("10.30.15.200","10.30.55.200")
